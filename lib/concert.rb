a57d59f77bb656f4c9756b7107adfcd0f8d3dfaa
Chris Gullickson <cgullickson5@gmail.com>
2017-12-16 21:38:55 +0000

    added concert class

--- lib/concert.rb
+++ lib/concert.rb
@@ -1,3 +1,18 @@
 require "bundler/setup"
 require "upcoming_concerts"
 require "nokogiri"
+
+class UpcomingConcerts::Concert
+  attr_accessor :artist, :showtime, :price, :ticket_url, :date
+
+  def initialize
+
+  end
+
+  def self.today
+    #return upcoming Concerts
+    self.scrape_concerts
+
+  end
+
+end
