78669127ece565c687bcd293b66f05b9dc608e84
Chris Gullickson <cgullickson5@gmail.com>
2017-12-19 23:28:52 +0000

    added ticket_url scraping and reworded some of the CLI

--- lib/upcoming/cli.rb
+++ lib/upcoming/cli.rb
@@ -8,7 +8,7 @@ class Upcoming::CLI
 
   def list_concerts
 
-    puts "Denver's Upcoming Concerts:"
+    puts "Tonight's Biggest Concerts in Denver:"
     @concerts = Upcoming::Concert.today
     @concerts.each.with_index(1) do |concert, i|
       puts "#{i}. #{concert.artist}"
@@ -19,14 +19,19 @@ class Upcoming::CLI
   def menu
     input = nil
     while input != "exit"
-      puts "Which concert would you like more information about? Please enter 1-5, list concerts, or exit."
+      puts "Which concert would you like more information about? "
+      puts "Please enter 1-5, list concerts, or exit."
       input = gets.strip.downcase
 
       if input.to_i > 0
         concert = @concerts[input.to_i - 1]
-        puts "#{input}. #{concert.artist} - #{concert.price} - #{concert.showtime} - #{concert.location} - "
+        puts "#{input}. #{concert.artist} - #{concert.price} - #{concert.showtime} - #{concert.location}"
+        puts "For more info and ticketing information,"
+        puts "visit #{concert.ticket_url}"
       elsif input == "list concerts"
         list_concerts
+      elsif input == "exit"
+        nil
       else
         puts "Not sure what you want, please enter 1-5, list deals, or exit"
       end
--- lib/upcoming/concert.rb
+++ lib/upcoming/concert.rb
@@ -36,6 +36,7 @@ class Upcoming::Concert
     concert_1.showtime = doc.css(".time")[1].text.strip
     concert_1.location = doc.css(".location")[1].text.strip
     concert_1.price = doc.css(".price")[1].text
+    concert_1.ticket_url = "westword.com" + doc.css(".title").css("a").attr("href").value
 
     concert_1
 
@@ -50,6 +51,7 @@ class Upcoming::Concert
     concert_2.showtime = doc.css(".time")[2].text.strip
     concert_2.location = doc.css(".location")[2].text.strip
     concert_2.price = doc.css(".price")[2].text
+    concert_2.ticket_url = "westword.com" + doc.css(".title")[2].css("a").attr("href").value
 
     concert_2
 
@@ -64,6 +66,7 @@ class Upcoming::Concert
     concert_3.showtime = doc.css(".time")[3].text.strip
     concert_3.location = doc.css(".location")[3].text.strip
     concert_3.price = doc.css(".price")[3].text
+    concert_3.ticket_url = "westword.com" + doc.css(".title")[3].css("a").attr("href").value
 
     concert_3
 
@@ -78,6 +81,7 @@ class Upcoming::Concert
     concert_4.showtime = doc.css(".time")[4].text.strip
     concert_4.location = doc.css(".location")[4].text.strip
     concert_4.price = doc.css(".price")[4].text
+    concert_4.ticket_url = "westword.com" + doc.css(".title")[4].css("a").attr("href").value
 
     concert_4
 
@@ -92,6 +96,7 @@ class Upcoming::Concert
     concert_5.showtime = doc.css(".time")[5].text.strip
     concert_5.location = doc.css(".location")[5].text.strip
     concert_5.price = doc.css(".price")[5].text
+    concert_5.ticket_url = "westword.com" + doc.css(".title")[5].css("a").attr("href").value
 
     concert_5
 
